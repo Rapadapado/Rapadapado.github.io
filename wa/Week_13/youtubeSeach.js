f5052ba39ffab2f68fb42643ccf9d8fa80fb61ea
Rapadapado <92283645+Rapadapado@users.noreply.github.com>
2025-11-16 11:08:17 -0700

    test

--- wa/Week_13/youtubeSeach.js
+++ wa/Week_13/youtubeSeach.js
@@ -11,7 +11,7 @@ fetch(video + new URLSearchParams({
     key: api_key,
     part: 'snippet',
     chart: 'mostPopular',
-    maxResult: 1,
+    maxResults: 30,
     regionCode: 'US'
 }))
 .then(res => res.json())
@@ -21,7 +21,7 @@ fetch(video + new URLSearchParams({
         getChannelIcon(item);
     })
 })
-.catch(err => console)
+.catch(err => console.log(err))
 
 const getChannelIcon = (video_data) => {
     fetch(channel + new URLSearchParams({
@@ -31,17 +31,19 @@ const getChannelIcon = (video_data) => {
     })) 
     .then(res => res.json())
     .then(data => {
+        console.log(data);
         video_data.channelThumbnail = data.items[0].snippet.thumbnails.default.url;
+        console.log(video_data);
         makeVideoCard(video_data);
     })
 }
 
 const makeVideoCard = (data) => {
     videoCard.innerHTML += `
-    <div class="video" onclick="location.href = 'https://youtube.com/watch?v=${data.id}>
-        <img scr="${data.snippet.thumbnails.high.url}" class="thumbnail" alt="">
+    <div class="video" onclick="location.href = 'https://youtube.com/watch?v=${data.id}'">
+        <img src="${data.snippet.thumbnails.high.url}" class="thumbnail" alt="">
             <div class="content">
-                <img scr="${data.channelThumbnail}" class="channel" alt=""> 
+                <img src="${data.channelThumbnail}" class="channel" alt=""> 
                 <div class="info">
                     <h4 class="title">${data.snippet.title}</h4>
                     <p class="channel_name">${data.snippet.channelTitle}</p>
