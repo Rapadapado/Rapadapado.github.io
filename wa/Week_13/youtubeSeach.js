04d4e0ebdb428ba39087a4c03736c6fc06bf516b
Rapadapado <92283645+Rapadapado@users.noreply.github.com>
2025-11-16 20:02:33 -0700

    test

--- wa/Week_13/youtubeSeach.js
+++ wa/Week_13/youtubeSeach.js
@@ -6,12 +6,14 @@ let video = "https://www.googleapis.com/youtube/v3/videos?";
 
 let channel= "https://www.googleapis.com/youtube/v3/channels?";
 
+let search= "https://www.googleapis.com/youtube/v3/search?"
+
 
 fetch(video + new URLSearchParams({
     key: api_key,
     part: 'snippet',
     chart: 'mostPopular',
-    maxResults: 30,
+    maxResults: 32,
     regionCode: 'US'
 }))
 .then(res => res.json())
@@ -57,8 +59,74 @@ const searchInput = document.querySelector('.searchbar');
 const searchBtn = document.querySelector('.searchbtn');
 let searchLink = "https://www.youtube.com/results?search_query=";
 
+
 searchBtn.addEventListener('click', () => {
     if(searchInput.value.length){
         location.href = searchLink + searchInput.value;
     }
 })
+
+/*searchBtn.addEventListener('click', () => {
+    /*if(searchInput.value.length){
+        location.href = search + searchInput.value;
+    }
+    searchByKeyword();
+})*/
+
+/*document.addEventListener('DOMContentLoaded', function () {
+    searchByKeyword();
+});
+
+
+function searchByKeyword() {
+  var results = YouTube.Search.list({
+    key: api_key,
+    part: 'snippet',
+    maxResults: 32,
+    order: 'date',
+    type: 'video',
+    q: searchInput
+  })
+  .then(res => res.json())
+  .then(data => {
+    console.log(data);
+  })
+
+  for(var i in results.items) {
+    var item = results.items[i];
+    Logger.log('[%s] Title: %s', item.id.videoId, item.snippet.title);
+  }
+}*/
+
+/*function searchByKeyword() {
+    const apiKey = 'search';
+    const searchInput = document.
+        getElementById('searchInput').value;
+
+    const videoCard = document.
+        getElementById('.video_content');
+
+    if (searchInput.trim() !== '') {
+
+        videoCard.innerHTML = '<p>Loading...</p>';
+
+        fetch(`search${apiKey}&s=${searchInput}`)
+            .then(response => response.json())
+            .then(data => {
+                if (data.Search && data.Search.length > 0) {
+                    makeVideoCard(data.Search);
+                } else {
+                    MoviesGrid.innerHTML =
+             '<p>No movies found with the given name!</p>';
+                }
+            })
+            .catch(error => {
+                console.error('Error fetching data:', error);
+                MoviesGrid.innerHTML =
+           '<p>Error fetching movies. Please try again later.</p>';
+            });
+    } else {
+        alert('Enter a movie title then search!');
+    }
+}*/
+
