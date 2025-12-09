8b441bc2c18ce1296989d1c760096f5afc309048
Rapadapado <92283645+Rapadapado@users.noreply.github.com>
2025-12-08 22:55:23 -0700

    test

--- wa/Final/Final.js
+++ wa/Final/Final.js
@@ -8,3 +8,13 @@
  document.querySelector('.Popup_Image span').onclick = () =>{
     document.querySelector('.Popup_Image').style.display = 'block';
  }
+
+let menuIcon = document.querySelector('#menu');
+let navbar = document.querySelector('.navBar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('X')
+    navbar.classList.toggle('active');
+};
+
+menuIcon.classList.remove('active')
