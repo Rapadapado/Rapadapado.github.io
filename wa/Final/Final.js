a26910ea1e207e61ec35a2b37f6efd5388b093e0
Rapadapado <92283645+Rapadapado@users.noreply.github.com>
2025-12-08 23:36:02 -0700

    Final

--- wa/Final/Final.js
+++ wa/Final/Final.js
@@ -18,3 +18,22 @@ menuIcon.onclick = () => {
 };
 
 menuIcon.classList.remove('active')
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('nav');
+
+window.onscroll = () =>{
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let diff = sec.offsetTop - 100;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= diff && top < (diff + height)){
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector(nav)
+            })
+        }
+    })
+}
