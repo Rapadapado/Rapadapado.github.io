da14d520a158cc703d9d83ad8c3120b3ce3cfce4
Rapadapado <92283645+Rapadapado@users.noreply.github.com>
2025-12-08 20:45:41 -0700

    test

--- wa/Final/Final.js
+++ wa/Final/Final.js
@@ -1,1 +1,10 @@
- 
+ document.querySelectorAll('.Project_Pictures img').forEach(image =>{
+    image.onclick = () =>{
+        document.querySelector('.Popup_Image').style.display = 'block';
+        document.querySelector('.Popup_image img').src = image.getAttribute('src');
+    }
+ });
+
+ document.querySelector('.Popup_Image span').onclick = () =>{
+    document.querySelector('.Popup_Image').style.display = 'block';
+ }
